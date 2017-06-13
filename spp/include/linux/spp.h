5f29c6a389343e84c8f075e080a661e2f11e4d41
Jacob Holtom <jacob@et.byu.edu>
2017-06-13 12:22:37 -0600

    restructured includes for linux 2.6.33

---
+++ spp/include/linux/spp.h
@@ -0,0 +1,26 @@
+/* 
+ * Public elements of the kernel SPP code.
+ *
+ * Jacob Holtom
+ */
+
+#include <linux/socket.h>
+
+#define SPP_MTU 65535 /* can be max length of 64K */
+
+typedef struct {
+    int spp_apid : 11; /* 11 bit APID */
+    /* Done in a struct to facilitate expansion to other addressing parameters (unlikely) */
+} spp_address;
+/* May need to add TM/TC parameter here, 1 bit flag */
+
+struct sockaddr_spp {
+    __kernel_sa_family_t sspp_family;
+    spp_address sspp_addr;
+};
+
+struct spp_facilities_struct {
+    spp_address s_addr, d_addr;
+    unsigned int rand;
+    spp_address fail_addr;
+};
