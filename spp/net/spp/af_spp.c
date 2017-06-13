b3856635a07c4cae28da2b524898479482a14640
Jacob Holtom <jacob@et.byu.edu>
2017-06-13 13:58:48 -0600

    added stub returns, removed too modern code

--- spp/net/spp/af_spp.c
+++ spp/net/spp/af_spp.c
@@ -126,7 +126,7 @@ static int spp_listen(struct socket *sock, int backlog)
     if(sk->sk_state != TCP_LISTEN){ /* If it is not already in a listening state */
         struct spp_sock *spp = spp_sk(sk); /* Get the SPP specific representation */
 
-        memset(&spp->d_addr, 0, SPP_ADDR_LEN); /* Set the destination address */
+        memset(&spp->d_addr, 0, SPP_ADDR_LEN); /* TODO: should we really be setting it to 0? why memset, shouldn't we just reference the null_addr? */
         sk->sk_max_ack_backlog = backlog; /* Adjust backlog for acknowledgements (SPP technically doesn't have this)*/
         sk->sk_state = TCP_LISTEN; /* Set state into listen */
         return 0; /* We did it! */
@@ -156,28 +156,15 @@ static int spp_create(struct net *net, struct socket *sock, int protocol, int ke
     if(sock->type != SOCK_SEQPACKET || protocol != 0)
         return -ESOCKTNOSUPPORT;
 
-    sk = sk_alloc(net, PF_SPP, GFP_ATOMIC, &spp_proto,kern);
+    sk = sk_alloc(net, AF_SPP, GFP_ATOMIC, &spp_proto);
     if (sk == NULL)
         return -ENOMEM;
     spp = spp_sk(sk);
     sock_init_data(sock, sk);
-    skb_queue_head_init(&spp->ack_queue);
-#ifdef M_BIT
-    skb_queue_head_init(&spp->frag_queue);
-    spp->fraglen = 0;
-#endif
-    sock->ops = &spp_proto_ops;
-    sk->sk_protocol = protocol;
 
     init_timer(&spp->timer);
     init_timer(&spp->idletimer);
-
-    spp->t1 = msecs_to_jiffies(sysctl_spp_call_request_timeout);
-    spp->t2 = msecs_to_jiffies(sysctl_spp_reset_request_timeout);
-    spp->t3 = msecs_to_jiffies(sysctl_spp_clear_request_timeout);
-    spp->idle = msecs_to_jiffies(sysctl_spp_no_activity_timeout);
-
-    spp->state = SPP_STATE_0;
+    /* TODO: rewrite so that it actually adjusts all the appropriate things */ 
 
     return 0;
 }
@@ -188,6 +175,7 @@ static int spp_create(struct net *net, struct socket *sock, int protocol, int ke
 static int spp_device_event(struct notifier_block *this, unsigned long event, void *ptr)
 {
     /* TODO: Implement device status change handling */
+    return 0;
 }
 
 /*
@@ -196,6 +184,7 @@ static int spp_device_event(struct notifier_block *this, unsigned long event, vo
 static int spp_release(struct socket *sock)
 {
     /* TODO: Implement socket release */
+    return 0;
 }
 
 /*
@@ -224,6 +213,9 @@ static int spp_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len)
    release_sock(sk);
    SOCK_DEBUG(sk, "spp_bind: socket is bound\n");
     /* TODO: Implement socket bind */
+
+out:
+   return rc;
 }
 
 /*
