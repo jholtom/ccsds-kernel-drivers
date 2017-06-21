9c2d6648838c621974028e9d38311e2a2a30ae7e
Jacob Holtom <jacob@holtom.me>
2017-06-20 19:15:22 -0600

    Updated Licensing/History/Attribution Header

--- spp/net/spp/sysctl_net_spp.c
+++ spp/net/spp/sysctl_net_spp.c
@@ -1,4 +1,22 @@
-/* sysctl for SPP - Jacob Holtom */
+/*
+ *    Space Packet Protocol Packet Layer release 001
+ *
+ *    This is BETA software, it may break your machine, fail randomly and
+ *    maybe have a lot of problems.  It works enough that its going to space.
+ *
+ *    This module:
+ *              This module is free software; you can redistribute it and/or
+ *              modify it under the terms of the GNU General Public License
+ *              as published by the Free Software Foundation; either version
+ *              2 of the License, or (at your option) any later version.
+ *
+ *      History
+ *      SPP 001         Jacob Holtom and Jacob Willis   Wrote the initial implementation
+ *
+ *      Authors: Jacob Holtom <jacob@holtom.me>
+ *               Jacob Willis <willisj2@byu.edu>
+ *
+ */
 #include <linux/mm.h>
 #include <linux/sysctl.h>
 #include <linux/init.h>
