129b3600ef44cd40d0c070ad495bdd517cb2c9e1
Jacob Holtom <jacob@holtom.me>
2017-06-27 12:35:56 -0600

    fixed device location

--- spp/net/spp/spp_dev.c
+++ spp/net/spp/spp_dev.c
@@ -53,7 +53,7 @@ struct spp_dev *spp_addr_sppdev(spp_address *addr)
     for(spp_dev = spp_dev_list; spp_dev != NULL; spp_dev = spp_dev->next)
     {
         for(s = spp_dev->ifa_list; s != NULL; s = s->ifa_next){
-            if((addr->spp_apid = s->ifa_address) == 0){
+            if(addr->spp_apid == s->ifa_address){
                 res = spp_dev;
             }
         }
