798c8ea1b1052ed2535aa251a9fa6df916b6a7c0
Jacob Holtom <jacob@holtom.me>
2017-06-27 14:15:22 -0600

    fixed skb_under_panic

--- spp/net/spp/af_spp.c
+++ spp/net/spp/af_spp.c
@@ -489,7 +489,7 @@ static int spp_sendmsg(struct kiocb *iocb, struct socket *sock, struct msghdr *m
     /* Handle packets in a sequence here */
     /* determine this if the data length is greater than 1k */
 
-    skb_push(skb, 2 + sizeof(struct spphdr));
+    skb_push(skb, sizeof(struct spphdr));
     hdr = spp_hdr(skb);
     hdr->pvn = 0;
     hdr->pt = 0; /*TODO: configure this to actually be able to swithc between TM and TC */
